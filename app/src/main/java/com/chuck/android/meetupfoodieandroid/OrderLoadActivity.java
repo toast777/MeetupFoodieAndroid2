4878de3b187f769dc3c57ff9da7e470d7ffd3922
Chuck Robinson <gdgkctest@users.noreply.github.com>
2018-10-04 22:31:45 -0500

    Working on Skeleton Navigation

--- app/src/main/java/com/chuck/android/meetupfoodieandroid/OrderLoadActivity.java
+++ app/src/main/java/com/chuck/android/meetupfoodieandroid/OrderLoadActivity.java
@@ -1,5 +1,6 @@
 package com.chuck.android.meetupfoodieandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,4 +28,8 @@ public class OrderLoadActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void startNewOrder(View view) {
+        Intent intent = new Intent(getApplicationContext(), OrderStartActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/chuck/android/meetupfoodieandroid/StartActivity.java
+++ app/src/main/java/com/chuck/android/meetupfoodieandroid/StartActivity.java
@@ -1,11 +1,18 @@
 package com.chuck.android.meetupfoodieandroid;
 
+
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.chuck.android.meetupfoodieandroid.utils.FirebaseLogin;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class StartActivity extends AppCompatActivity {
     public static final String PREF_REGION = "Region Preference";
@@ -14,7 +21,18 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       FirebaseApp.initializeApp(this);
+
         setContentView(R.layout.activity_start);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+
+
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
         //Check if region set
@@ -23,13 +41,14 @@ public class StartActivity extends AppCompatActivity {
             editor.putString(PREF_REGION, "Kansas City");
             editor.apply();
         }
-        //Log in Anonymous
+        //Log in Anonymous - more auth choices in future
         FirebaseLogin fLogin = new FirebaseLogin();
 
         //Goto to next Activity if Firebase Logged in and SharedPref set
         if (sharedPreferences.contains(PREF_REGION) && (fLogin.getCurrentUser() != null))
         {
-            //Goto next activity
+            Intent intent = new Intent(getApplicationContext(), OrderLoadActivity.class);
+            startActivity(intent);
         }
 
 
